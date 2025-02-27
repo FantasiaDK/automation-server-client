62d2d96db2183ca4b4c2ba7ab6fd4636aa8f32bd
Michael Wulff Nielsen <mwulff@gmail.com>
2025-02-27 20:49:56 +0100

    Add WorkItemError to the public API of automation_server_client

--- src/automation_server_client/__init__.py
+++ src/automation_server_client/__init__.py
@@ -1,6 +1,6 @@
 
 from ._config import AutomationServerConfig
-from ._server import AutomationServer
+from ._server import AutomationServer, WorkItemError
 from ._models import Session, Process, Workqueue
 
-__all__ = ['AutomationServerConfig', 'AutomationServer', 'Session', 'Process', 'Workqueue']
+__all__ = ['AutomationServerConfig', 'AutomationServer', 'Session', 'Process', 'Workqueue','WorkItemError']
